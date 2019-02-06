bc54b25078dc04a76755ab755660a4a8a71301d0
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-02-06 23:23:53 +0530

    Built blocked path for a couple

--- separationTime.js
+++ separationTime.js
@@ -1,3 +1,4 @@
+// This function is the helper function to help compare the relative order of wall's appearance
 const compareElements = (wall1, wall2) => {
   if (wall1[2] < wall2[2]) {
     return -1;
@@ -7,4 +8,35 @@ const compareElements = (wall1, wall2) => {
   return 1;
 };
 
-module.exports = { compareElements };
+// This function actally sorts the wall on the basis of their appearance time
+const sortWallsByTime = (arrayOfWalls) => {
+  const copyOfarrayOfWalls = arrayOfWalls;
+  copyOfarrayOfWalls.sort(compareElements);
+  return copyOfarrayOfWalls;
+};
+
+// This function builds the blocked path for a couple
+const buildBlockedPath = (arrayOfWalls, walkStartTime) => {
+  // End of wall stores the last extent of wall
+  let endOfLastWall = 0;
+  // This part calculates the end of last wall
+  arrayOfWalls.forEach((x) => {
+    if (x[1] > endOfLastWall) {
+      [, endOfLastWall] = x;
+    }
+  });
+  // Initializing the array with 0 till the end of path
+  const blockedPath = Array(endOfLastWall + 1).fill(0);
+  // Filling all the section of path with blocker as 1
+  arrayOfWalls.forEach((wall) => {
+    const currentLocation = Math.max((wall[2] - walkStartTime), 0);
+    let [blockerStart] = wall;
+    if (blockerStart < currentLocation) {
+      blockerStart = currentLocation;
+    }
+    blockedPath.fill(1, blockerStart, wall[1]);
+  });
+  return blockedPath;
+};
+
+module.exports = { compareElements, sortWallsByTime, buildBlockedPath };
--- separationTime.test.js
+++ separationTime.test.js
@@ -1,4 +1,4 @@
-const { compareElements } = require('./separationTime');
+const { compareElements, sortWallsByTime, buildBlockedPath } = require('./separationTime');
 
 describe('compareElements () :', () => {
   it('should return -1 if two arrays are sorted in ascending order by their third element', () => {
@@ -9,5 +9,44 @@ describe('compareElements () :', () => {
   });
   it('should return 1 if two arrays are sorted in descending order by their third element', () => {
     expect(compareElements([1, 2, 3], [10, 13, 1])).toEqual(1);
+  });
+});
+
+describe('sortWallsByTime () :', () => {
+  const input = [[1, 3, 2], [7, 1, 10], [1, 4, 1], [5, 4, 3]];
+  const output = [[1, 4, 1], [1, 3, 2], [5, 4, 3], [7, 1, 10]];
+  it('should return the elements sorted by their third element', () => {
+    expect(sortWallsByTime(input)).toEqual(output);
+  });
+  const input1 = [[7, 1, 10], [5, 4, 3], [1, 3, 2], [1, 4, 1]];
+  const output1 = [[1, 4, 1], [1, 3, 2], [5, 4, 3], [7, 1, 10]];
+  it('should return the elements sorted by their thirdelement which were reverse sorted', () => {
+    expect(sortWallsByTime(input1)).toEqual(output1);
+  });
+  const input2 = [[1, 4, 1], [1, 3, 2], [5, 4, 3], [7, 1, 10]];
+  const output2 = [[1, 4, 1], [1, 3, 2], [5, 4, 3], [7, 1, 10]];
+  it('should return the array which was already sorted', () => {
+    expect(sortWallsByTime(input2)).toEqual(output2);
+  });
+});
+
+describe('buildBlockedPath () :', () => {
+  it('should return an array representing the points where couple do not see each other', () => {
+    const input1 = [[5, 7, 3], [2, 5, 4], [3, 6, 5]];
+    const input2 = 0;
+    const output = [0, 0, 0, 0, 1, 1, 1, 0];
+    expect(buildBlockedPath(input1, input2)).toEqual(output);
+  });
+  it('should return an array representing the points where couple do not see each other', () => {
+    const input1 = [[5, 7, 3], [2, 5, 4], [3, 6, 5]];
+    const input2 = 1;
+    const output = [0, 0, 0, 1, 1, 1, 1, 0];
+    expect(buildBlockedPath(input1, input2)).toEqual(output);
+  });
+  it('should return an array representing the points where couple do not see each other', () => {
+    const input1 = [[5, 7, 3], [2, 5, 4], [3, 6, 5]];
+    const input2 = 2;
+    const output = [0, 0, 1, 1, 1, 1, 1, 0];
+    expect(buildBlockedPath(input1, input2)).toEqual(output);
   });
 });
